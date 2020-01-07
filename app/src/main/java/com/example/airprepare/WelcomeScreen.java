1a3d02fc4e6dda1d9a1c125dadd4d31a0aa20f46
jaya phanindra <jayaphanindra686@gmail.com>
2020-01-07 17:18:57 +0530

    App after UI implementation and opening links in same app airprepare

---
+++ app/src/main/java/com/example/airprepare/WelcomeScreen.java
@@ -0,0 +1,30 @@
+package com.example.airprepare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        int welcome_time = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(WelcomeScreen.this, MainActivity.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, welcome_time);
+
+    }
+}
